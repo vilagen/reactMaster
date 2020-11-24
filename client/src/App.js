e106fde958e8ce7f2b5762a65c2999091e23e86c
vilagen <vilagen@yahoo.com>
2020-11-24 07:21:51 -0500

    added preview-components.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component.jsx';
 
 const HatsPage = () => {
   return (
@@ -17,7 +18,8 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        {/* <Route path='/shop/hats' component={HatsPage} /> */}
+        <Route path='/shop' component={ShopPage} />
       </Switch>
 
     </div>
