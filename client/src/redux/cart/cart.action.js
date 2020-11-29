1c6117ef8411d8b2c4385dbfd41761baaac51d34
vilagen <vilagen@yahoo.com>
2020-11-29 18:36:29 -0500

    Added cartIcon and cartDropdown components, implemented cart reducer.

---
+++ client/src/redux/cart/cart.action.js
@@ -0,0 +1,7 @@
+import CartActionTypes from './cart.types';
+
+// we don't need a payload, because the reducer is set to change 
+// the state from true to false automatically
+export const toggleCartHidden = () => ({
+  type: CartActionTypes.TOGGLE_CART_HIDDEN
+})
