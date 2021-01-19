511a154a9b024349078202085417c6e0f336d66b
vilagen <vilagen@yahoo.com>
2021-01-19 14:28:15 -0500

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new ux flow for directory/shop

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,27 +1,61 @@
-import React from 'react';
+import React from "react";
+// import { connect } from "react-redux";
+// import { createStructuredSelector } from "reselect";
+import { Route } from "react-router-dom";
 
-import SHOP_DATA from './shop.data.js';
+// import SHOP_DATA from "../../redux/shop/shop.data.js";
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+// import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../collection/collection.component";
+// import { selectCollections } from "../../redux/shop/shop.selectors";
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-
-  render() {
-    const {collections} = this.state;
-    return ( 
-      <div className="shop-page"> 
-      {collections.map(({id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
-    </div>)
-  }
+// because App page this is being used with is under a Route, route passes 3 components, match, location, and history.
+const ShopPage = ({ match, collections }) => {
+  console.log(match.path);
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+  );
 };
 
 export default ShopPage;
+
+// const ShopPage = ({ collections }) => {
+//   return (
+//     <div className="shop-page">
+//       {collections.map(({ id, ...otherCollectionProps }) => (
+//         <CollectionPreview key={id} {...otherCollectionProps} />
+//       ))}
+//     </div>
+//   );
+// };
+
+// const mapStateToProps = createStructuredSelector({
+//   collections: selectCollections,
+// });
+
+// class ShopPage extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       collections: SHOP_DATA,
+//     };
+//   }
+
+//   render() {
+//     const { collections } = this.state;
+//     return (
+//       <div className="shop-page">
+//         {collections.map(({ id, ...otherCollectionProps }) => (
+//           <CollectionPreview key={id} {...otherCollectionProps} />
+//         ))}
+//       </div>
+//     );
+//   }
+// }
+
+// export default connect(mapStateToProps)(ShopPage);
---
+++ client/src/redux/shop/shop.selectors.js
@@ -0,0 +1,34 @@
+import { createSelector } from "reselect";
+
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5,
+};
+
+const selectShop = (state) => state.shop;
+
+export const selectCollections = createSelector(
+  [selectShop],
+  (shop) => shop.collections
+);
+
+//because our shop data is an object, but we use a map on collection-overview page to list out the items,
+// we are collecting a list of keys, then usin map function to create a new array for collection-overview to use.
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+);
+
+export const selectCollection = (collectionUrlParam) =>
+  // createSelector([selectCollections], (collections) =>
+  // collections.find(
+  //   (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+  // )
+  // after updating the array on shop.data.js to a full object:
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
+  );
